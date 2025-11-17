116e466c915db3f803e07454c60467fabc0a64fb
Jacopo Andrea Giola <jacopo@giola.org>
2025-11-17 16:26:14 +0100

    add initial source interface

---
+++ internal/source/interface.go
@@ -0,0 +1,23 @@
+// Copyright Mia srl
+// SPDX-License-Identifier: AGPL-3.0-only or Commercial
+
+package source
+
+// SyncableSource defines the interface for a data source that supports synchronization operations.
+type SyncableSource interface {
+	// StartSyncProcess will be called to initiate a synchronization process for the data source.
+	// It receives a channel through which it can send the synched data or it can return an error
+	// if the synchronization process fails. typesToSync is a list of data types that need to be
+	// synchronized.
+	StartSyncProcess(typesToSync []string, results chan<- SourceData) (err error)
+}
+
+// EventSource defines the interface for a data source that uses event-driven mechanisms to handle
+// data updates.
+type EventSource interface {
+	// StartEventStream will be called to initiate an event stream for the data source.
+	// It receives a channel through which it can send the incoming data or it can return an error
+	// if the event stream fails. typesToStream is the list of data types that is expected to be
+	// returned.
+	StartEventStream(typesToStream []string, results chan<- SourceData) (err error)
+}
