0e8f07aa7203cccb0c06318ee68c76c1a97e5bf6
Jacopo Andrea Giola <jacopo@giola.org>
2025-11-18 17:57:30 +0100

    feat: add extension support for building on windows systems

--- tools/make/build.mk
+++ tools/make/build.mk
@@ -31,11 +31,12 @@ go/build/%:
 	$(eval OS:= $(word 1,$(subst /, ,$*)))
 	$(eval ARCH:= $(word 2,$(subst /, ,$*)))
 	$(eval ARM:= $(word 3,$(subst /, ,$*)))
+	$(eval EXT:= $(if $(filter windows,$(OS)),.exe,))
 	$(info Building image for $(OS) $(ARCH) $(ARM))
 
 	mkdir -p "$(BUILD_OUTPUT)/$(OS)/$(ARCH)$(if $(ARM),/v$(ARM),)"
 	GOOS=$(OS) GOARCH=$(ARCH) GOARM=$(ARM) CGO_ENABLED=0 go build -trimpath \
-		-ldflags "$(GO_LDFLAGS)" -o $(BUILD_OUTPUT)/$(OS)/$(ARCH)$(if $(ARM),/v$(ARM),)/$(CMDNAME) $(BUILD_PATH)
+		-ldflags "$(GO_LDFLAGS)" -o $(BUILD_OUTPUT)/$(OS)/$(ARCH)$(if $(ARM),/v$(ARM),)/$(CMDNAME)$(EXT) $(BUILD_PATH)
 
 .PHONY: build
 build: clean/bin go/build/$(GOOS)/$(GOARCH)/$(GOARM)
