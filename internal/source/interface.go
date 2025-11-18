b726cc7f653ae1c5d4b796c54ba29194a4a903b7
Jacopo Andrea Giola <jacopo@giola.org>
2025-11-18 17:01:59 +0100

    add new ClosableSource interface

--- internal/source/interface.go
+++ internal/source/interface.go
@@ -5,6 +5,7 @@ package source
 
 import (
 	"context"
+	"time"
 )
 
 // SyncableSource defines the interface for a data source that supports synchronization operations.
@@ -24,4 +25,13 @@ type EventSource interface {
 	// if the event stream fails. typesToStream is the list of data types that is expected to be
 	// returned.
 	StartEventStream(ctx context.Context, typesToStream []string, results chan<- SourceData) (err error)
+}
+
+// ClosableSource defines the interface for a data source that can be gracefully closed. It will
+// be called when the source is being shut down to allow it to release any held resources. It receives
+// a context and a timeout duration to ensure the close operation does not hang indefinitely.
+// The close method can be called both during normal shutdown or for restarting the source.
+type ClosableSource interface {
+	// Close will be called to gracefully shut down the data source, releasing any resources it holds.
+	Close(ctx context.Context, timeout time.Duration) (err error)
 }
