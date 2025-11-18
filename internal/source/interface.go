7bd0f86689d248a8a48be0bef637d53c00cab0b6
Jacopo Andrea Giola <jacopo@giola.org>
2025-11-18 17:01:44 +0100

    add context to source interface methods

--- internal/source/interface.go
+++ internal/source/interface.go
@@ -3,13 +3,17 @@
 
 package source
 
+import (
+	"context"
+)
+
 // SyncableSource defines the interface for a data source that supports synchronization operations.
 type SyncableSource interface {
 	// StartSyncProcess will be called to initiate a synchronization process for the data source.
 	// It receives a channel through which it can send the synched data or it can return an error
 	// if the synchronization process fails. typesToSync is a list of data types that need to be
 	// synchronized.
-	StartSyncProcess(typesToSync []string, results chan<- SourceData) (err error)
+	StartSyncProcess(ctx context.Context, typesToSync []string, results chan<- SourceData) (err error)
 }
 
 // EventSource defines the interface for a data source that uses event-driven mechanisms to handle
@@ -19,5 +23,5 @@ type EventSource interface {
 	// It receives a channel through which it can send the incoming data or it can return an error
 	// if the event stream fails. typesToStream is the list of data types that is expected to be
 	// returned.
-	StartEventStream(typesToStream []string, results chan<- SourceData) (err error)
+	StartEventStream(ctx context.Context, typesToStream []string, results chan<- SourceData) (err error)
 }
